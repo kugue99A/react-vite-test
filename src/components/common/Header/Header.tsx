41bfd1b2d01f680d1d4387cbd61d2dd64013b4dc
Kugue99A <hisabisa99yohey@gmail.com>
2022-06-26 16:39:42 +0900

    init

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx'
+
+const Header = () => {
+  return (
+    <>
+      <header className={clsx()}>
+        <div></div>
+      </header>
+    </>
+  )
+}
+
+export default Header
