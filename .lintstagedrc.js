0420949c8b9b0ea678c08ccd03c63bf4ded46ad7
Kugue99A <hisabisa99yohey@gmail.com>
2022-06-27 00:18:25 +0900

    init

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,6 @@ module.exports = {
     `prettier --write ${filenames.map((file) => file.split(process.cwd())[1].slice(1)).join(' ')}`,
   '**/*.{ts, tsx}': (filenames) =>
     `eslint ${filenames.map((file) => file.split(process.cwd())[1].slice(1)).join(' ')} --fix`,
-  '**/*.{css}': (filenames) =>
+  '**/*.css': (filenames) =>
     `stylelint ${filenames.map((file) => file.split(process.cwd())[1].slice(1)).join(' ')} --fix`,
 }
